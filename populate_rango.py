36d7258d7b3f31b0a4990f7bb2195606717ab6c2
Lewis Ponsonby <2464980P@student.gla.ac.uk>
2021-02-09 01:59:03 +0000

    Finished chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -10,25 +10,41 @@ from rango.models import Category, Page
 def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/'},
+         'url':'http://docs.python.org/3/tutorial/',
+         'views':'62',
+         'likes':'31'},
         {'title':'How to Think like a Computer Scientist',
-         'url':'http://www.greenteapress.com/thinkpython/'},
+         'url':'http://www.greenteapress.com/thinkpython/',
+         'views':'18',
+         'likes':'9'},
         {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/'} ]
+         'url':'http://www.korokithakis.net/tutorials/python/',
+         'views':'40',
+         'likes':'20'} ]
 
     django_pages = [
         {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+         'views': '48',
+         'likes':'24'},
         {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
+         'url':'http://www.djangorocks.com/',
+         'views':'18',
+         'likes':'9'},
         {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'} ]
+         'url':'http://www.tangowithdjango.com/',
+         'views':'16',
+         'likes':'8'} ]
 
     other_pages = [
         {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
+         'url':'http://bottlepy.org/docs/dev/',
+         'views':'12',
+         'likes':'6'},
         {'title':'Flask',
-         'url':'http://flask.pocoo.org'} ]
+         'url':'http://flask.pocoo.org',
+         'views':'14',
+         'likes':'7'} ]
 
     cats = {'Python': {'pages': python_pages},
             'Django': {'pages': django_pages},
@@ -39,12 +55,13 @@ def populate():
         c = add_cat(cat, initialn, initialn/2)
         initialn=initialn/2
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'],p['views'],p['likes'])
 
-def add_page(cat, title, url, views=0):
-    p = Page.objects.get_or_create(category=cat, title=title)[0]
+def add_page(cat, title, url, views=0, likes=0):
+    p = Page.objects.get_or_create(category=cat, title=title, views=views, likes=likes)[0]
     p.url=url
     p.views=views
+    p.likes=likes
     p.save()
     return p
 
--- rango/models.py
+++ rango/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    views = models.PositiveIntegerField(default=0)
-    likes = models.PositiveIntegerField(default=0)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+        
 
     class Meta:
         verbose_name_plural = "Categories"
@@ -16,6 +23,7 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
